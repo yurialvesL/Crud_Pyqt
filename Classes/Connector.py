e4fa2666f10c88f893d082d1a69e579625501a33
yuri timao <yuri-timao2011@hotmail.com>
2022-08-17 19:57:18 -0300

    Inclusion of functions "insert","update" and "delete" in this application

--- Classes/Connector.py
+++ Classes/Connector.py
@@ -28,11 +28,9 @@ def personcollection():
             cursor.execute(select_query)
 
             result = cursor.fetchall()
-            print(result)
 
             person_list = []
             for i in result:
-                print(i)
                 person = Person(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7])
                 person_list.append(person)
             cursor.close()
@@ -43,6 +41,80 @@ def personcollection():
         print("Error while connecting to MySQL", e)
 
 
-class Connector():
-    def __init__(self):
-        pass
+def insert_person(Person):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            port="3306",
+            user="root",
+            password="",
+            database="comercio"
+
+        )
+        if mydb.is_connected():
+            insert_cliquery = "INSERT INTO CLIENTE(NOME,SEXO,EMPREGO,EMAIL,CPF) VALUES(%s,%s,%s,%s,%s);"
+            insert_phonequery = "INSERT INTO TELEFONE(TIPO,NUMERO,IDCLIENTE) VALUES(%s,%s,%s);"
+            cursor = mydb.cursor()
+            values = (Person.name, Person.sex, Person.job, Person.email, Person.cpf)
+            cursor.execute(insert_cliquery, values)
+            values_phone = (Person.typephone, Person.phone_number, cursor.lastrowid)
+            cursor.execute(insert_phonequery, values_phone)
+            userid = cursor.lastrowid
+            mydb.commit()
+            cursor.close()
+            mydb.close()
+            print('foi cadastrado o novo usuario de ID:', userid)
+
+    except Error as e:
+        print(f'Erro in insert{e}')
+
+
+def update_person(Person):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            port="3306",
+            user="root",
+            password="",
+            database="comercio"
+
+        )
+        if mydb.is_connected():
+            cursor = mydb.cursor()
+            update_cliquery = "UPDATE CLIENTE SET NOME= %s,SEXO = %s, EMPREGO = %s, EMAIL = %s, CPF = %s WHERE " \
+                           "(ID_CLIENTE = %s); "
+            values = (Person.name,Person.sex,Person.job,Person.email,Person.cpf,Person.id)
+            update_telquery = "UPDATE TELEFONE SET TIPO = %s, Numero = %s WHERE (IDCLIENTE = %s); "
+            phone_values = (Person.typephone, Person.phone_number, cursor.lastrowid)
+            cursor.execute(update_cliquery,values)
+            cursor.execute(update_telquery,phone_values)
+            mydb.commit()
+            cursor.close()
+            mydb.close()
+            print('deu bom garai')
+    except Error as e:
+        print(e)
+
+def delete_person(id):
+    try:
+        mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            port="3306",
+            user="root",
+            password="",
+            database="comercio"
+
+        )
+        if mydb.is_connected():
+            cursor = mydb.cursor()
+            delete_phonequery = "DELETE FROM TELEFONE WHERE (IDCLIENTE = %s);"
+            delete_query = "DELETE FROM CLIENTE WHERE (ID_CLIENTE = %s);"
+            values = (id,)
+            cursor.execute(delete_phonequery,values)
+            cursor.execute(delete_query,values)
+            mydb.commit()
+
+    except Error as e:
+        print(e)
+
+
--- Classes/crud.py
+++ Classes/crud.py
@@ -1,22 +1,27 @@
-from PyQt5.QtWidgets import QMainWindow,QMessageBox,QRadioButton,QTableWidget
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QRadioButton, QTableWidget
+from PyQt5.QtGui import QIcon
 from Template.tela import *
 from Template.registration import *
 from Classes.Person import *
 from Classes.Connector import *
 
 
-class Crud(QMainWindow,Ui_MainWindow):
+class Crud(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.list = personcollection()
         super().setupUi(self)
-        self.insertperson()
+        self.table_person()
+        self.list =[]
+        self.btn_insert.clicked.connect(self.insertperson)
+        self.btn_update.clicked.connect(self.updata)
+        self.btn_delete.clicked.connect(self.delete)
 
-    def insertperson(self):
+    def table_person(self):
+        self.list = personcollection()
         self.qtable_contacts.setRowCount(len(self.list))
-        row=0
+        row = 0
         for i in self.list:
-            self.qtable_contacts.setItem(row,0,QtWidgets.QTableWidgetItem(str(i.id)))
+            self.qtable_contacts.setItem(row, 0, QtWidgets.QTableWidgetItem(str(i.id)))
             self.qtable_contacts.setItem(row, 1, QtWidgets.QTableWidgetItem(i.name))
             self.qtable_contacts.setItem(row, 2, QtWidgets.QTableWidgetItem(i.sex))
             self.qtable_contacts.setItem(row, 3, QtWidgets.QTableWidgetItem(i.job))
@@ -24,15 +29,154 @@ class Crud(QMainWindow,Ui_MainWindow):
             self.qtable_contacts.setItem(row, 5, QtWidgets.QTableWidgetItem(i.cpf))
             self.qtable_contacts.setItem(row, 6, QtWidgets.QTableWidgetItem(i.typephone))
             self.qtable_contacts.setItem(row, 7, QtWidgets.QTableWidgetItem(i.phone_number))
-            print(i.id,i.name,i.sex,i.job,i.email,i.cpf,i.typephone,i.phone_number)
-            row+=1
+            row += 1
+
+    def insertperson(self):
+        self.regis = Registration()
+        self.hide()
+        self.regis.show()
+
+    def updata(self):
+        rows = sorted(set(index.row() for index in
+                          self.qtable_contacts.selectedIndexes()))
+        id = self.qtable_contacts.item(rows[0], 0).text()
+        name = self.qtable_contacts.item(rows[0], 1).text()
+        sex = self.qtable_contacts.item(rows[0], 2).text()
+        job = self.qtable_contacts.item(rows[0], 3).text()
+        email = self.qtable_contacts.item(rows[0], 4).text()
+        cpf = self.qtable_contacts.item(rows[0], 5).text()
+        type_phone = self.qtable_contacts.item(rows[0], 6).text()
+        phone_number = self.qtable_contacts.item(rows[0], 7).text()
+        persona = Person(id, name, sex, job, email, cpf, type_phone, phone_number)
+        print(persona.name)
+        self.up = Update()
+        self.hide()
+        self.up.show()
+        self.up.filldata(persona)
+
+    def delete(self):
+        rows = sorted(set(index.row() for index in
+                          self.qtable_contacts.selectedIndexes()))
+        id = self.qtable_contacts.item(rows[0], 0).text()
+        if self.btn_delete.clicked:
+            message = QMessageBox()
+            message.setWindowIcon(QIcon('img/cross.png'))
+            message.setWindowTitle('Exclusão de contato')
+            message.setIcon(QMessageBox.Critical)
+            message.setText(f'Tem certeza que deseja excluir o usuário de ID:{id} ?')
+            message.setStandardButtons(QMessageBox.Yes | QMessageBox.Cancel)
+            returnvalue = message.exec()
+            if returnvalue == QMessageBox.Yes:
+                delete_person(id)
+                message = QMessageBox()
+                message.setWindowIcon(QIcon('img/cross.png'))
+                message.setWindowTitle('User information')
+                message.setIcon(QMessageBox.Information)
+                message.setText(f'User deleted with success')
+                message.setStandardButtons(QMessageBox.Ok)
+                message.exec()
+                self.table_person()
 
 
-class Registration(QMainWindow,Ui_Registration):
+
+
+class Registration(QMainWindow, Ui_Registration):
     def __init__(self, parent=None):
         super().__init__(parent)
         super().setupUi(self)
+        self.typephones()
+        self.btn_register.clicked.connect(self.insert)
+
+    def typephones(self):
+        list_type = ['CELL', 'COM', 'RES']
+        self.comboBox.addItems(list_type)
+
+    def insert(self):
+
+        Person.name = self.txt_name.text()
+        Person.job = self.txt_job.text()
+        if self.rbd_men.isChecked():
+            Person.sex = 'M'
+        elif self.rbd_woman.isChecked():
+            Person.sex = 'F'
+        Person.email = self.txt_email.text()
+        Person.cpf = self.txt_cpf.text()
+        option = self.comboBox.currentText()
+        if option == "CELL":
+            Person.typephone = "CELL"
+        elif option == "RES":
+            Person.typephone = "RES"
+        elif option == "COM":
+            Person.typephone = "COM"
+        Person.phone_number = self.txt_phonenumber.text()
+        insert_person(Person)
+        message = QMessageBox()
+        message.setWindowIcon(QIcon('img/agenda.png'))
+        message.setIcon(QMessageBox.Information)
+        message.setMinimumSize(600, 1000)
+        message.setText('User insert in Database with success!')
+        message.setStandardButtons(QMessageBox.Ok)
+        message.exec()
+        self.principal = Crud()
+        self.hide()
+        self.principal.show()
 
 
+class Update(QMainWindow, Ui_Registration):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+        list_type = ['CELL', 'COM', 'RES']
+        self.comboBox.addItems(list_type)
+        self.btn_register.clicked.connect(self.update_contact)
 
+    def filldata(self, Person):
+        self.txt_name.setText(Person.name)
+        self.txt_job.setText(Person.job)
+        self.txt_cpf.setText(Person.cpf)
+        self.txt_email.setText(Person.email)
+        self.txt_phonenumber.setText(Person.phone_number)
+        self.lbl_id.setText(Person.id)
+        if Person.typephone == "CELL":
+            self.comboBox.setCurrentText("CELL")
+        elif Person.typephone == "RES":
+            self.comboBox.setCurrentText('RES')
+        elif Person.typephone == "COM":
+            self.comboBox.setCurrentText('COM')
 
+        if Person.sex == "M":
+            self.rbd_men.setChecked(True)
+        elif Person.sex == "F":
+            self.rbd_woman.setChecked(True)
+
+    def update_contact(self):
+
+        Person.id = self.lbl_id.text()
+        Person.name = self.txt_name.text()
+        Person.job = self.txt_job.text()
+        Person.cpf = self.txt_cpf.text()
+        Person.email = self.txt_email.text()
+        Person.phone_number = self.txt_phonenumber.text()
+        option = self.comboBox.currentText()
+        if option == "CELL":
+            Person.typephone = "CELL"
+        elif option == "RES":
+            Person.typephone = "RES"
+        elif option == "COM":
+            Person.typephone = "COM"
+        if self.rbd_men.isChecked():
+            Person.sex = "M"
+        elif self.rbd_woman.isChecked():
+            Person.sex = "F"
+
+        update_person(Person)
+        message = QMessageBox()
+        message.setWindowIcon(QIcon('img/agenda.png'))
+        message.setIcon(QMessageBox.Information)
+        message.setMinimumSize(600, 1000)
+        message.setText('Update User Success!')
+        message.setStandardButtons(QMessageBox.Ok)
+        message.exec()
+        self.principal = Crud()
+        self.hide()
+        self.principal.show()
